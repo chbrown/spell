817ade0f54e54f4ef732bc15ab97a8bc5da683a2
Christopher Brown <io@henrian.com>
2019-01-05 11:37:16 -0600

    Add npm & webpack boilerplate (config)

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const mode = process.env.NODE_ENV || 'development'
+
+module.exports = {
+  mode,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+}
